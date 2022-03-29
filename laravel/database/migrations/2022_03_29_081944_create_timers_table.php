0419b05f4cadf182234b036b0062222c0c9e9493
YutoSeino <seino.y2001@gmail.com>
2022-03-30 01:48:09 +0900

    create timer

---
+++ laravel/database/migrations/2022_03_29_081944_create_timers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTimersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('time');
+            $table->bigInteger('elapsed_time');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timers');
+    }
+}
--- laravel/resources/views/layouts/task.blade.php
+++ laravel/resources/views/layouts/task.blade.php
@@ -7,6 +7,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/timer.js') }}" defer></script>
     @stack('css')
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
