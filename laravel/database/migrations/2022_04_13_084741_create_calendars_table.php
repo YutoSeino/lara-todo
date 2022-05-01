f152a520f14d376240e132127cedf0a36c568625
YutoSeino <seino.y2001@gmail.com>
2022-05-01 16:55:43 +0900

    migration 編集

--- laravel/database/migrations/2022_04_13_084741_create_calendars_table.php
+++ laravel/database/migrations/2022_04_13_084741_create_calendars_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSchedulesTable extends Migration
+class CreateCalendarsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateSchedulesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('schedules');
+        Schema::dropIfExists('calendars');
     }
 }
